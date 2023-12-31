7d2be053cab6dc9dcf881cd893ad29697ea50189
Sarthak Lad <135553451+Sarthaklad1034@users.noreply.github.com>
2023-12-31 13:37:19 +0530

    Update spmas.sql

--- spmas.sql
+++ spmas.sql
@@ -1,11 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 4.1.12
 -- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Apr 18, 2015 at 09:34 AM
--- Server version: 5.5.36
--- PHP Version: 5.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
